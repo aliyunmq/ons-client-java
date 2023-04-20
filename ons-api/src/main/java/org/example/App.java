7cf0ad24af3df3bdfd4a88af53d7f79150968289
Aaron Ai <yangkun.ayk@gmail.com>
2023-04-20 15:32:43 +0800

    Make it compile

--- ons-api/src/main/java/org/example/App.java
+++ ons-api/src/main/java/org/example/App.java
@@ -2,12 +2,11 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
     }
 }
