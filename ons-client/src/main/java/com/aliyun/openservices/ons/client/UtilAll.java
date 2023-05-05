228fa368e1338bc550445391ea0ee9ce58722054
Aaron Ai <yangkun.ayk@alibaba-inc.com>
2023-05-05 15:05:08 +0800

    Add private constructor for UtilAll

--- ons-client/src/main/java/com/aliyun/openservices/ons/client/UtilAll.java
+++ ons-client/src/main/java/com/aliyun/openservices/ons/client/UtilAll.java
@@ -4,4 +4,7 @@ public class UtilAll {
     public static final String DEFAULT_CHARSET = "UTF-8";
     public static final String HTTP_PREFIX = "http://";
     public static final String HTTPS_PREFIX = "https://";
+
+    private UtilAll() {
+    }
 }
