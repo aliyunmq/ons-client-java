7f89107a45af13fba67c8d758a03e5ee672199d3
Aaron Ai <yangkun.ayk@alibaba-inc.com>
2023-05-05 16:54:27 +0800

    Add DELAY/TRANSACTION message example

--- ons-client/src/main/java/com/aliyun/openservices/ons/client/example/ProducerNormalMessageExample.java
+++ ons-client/src/main/java/com/aliyun/openservices/ons/client/example/ProducerNormalMessageExample.java
@@ -23,7 +23,7 @@ public class ProducerNormalMessageExample {
         properties.put(PropertyKeyConst.SecretKey, "yourSecretKey");
         // 设置发送超时时间，单位：毫秒。
         properties.setProperty(PropertyKeyConst.SendMsgTimeoutMillis, "3000");
-        // 设置TCP接入域名，进入消息队列 RocketMQ 版控制台实例详情页面的接入点区域查看。
+        // 设置 TCP 接入域名，进入消息队列 RocketMQ 版控制台实例详情页面的接入点区域查看。
         properties.put(PropertyKeyConst.NAMESRV_ADDR, "yourNameSrvAddr");
         Producer producer = ONSFactory.createProducer(properties);
         // 在发送消息前，必须调用 start 方法来启动 Producer ，只需调用一次即可。
@@ -31,23 +31,22 @@ public class ProducerNormalMessageExample {
 
         String topic = "yourNormalTopic";
         String tag = "yourMessageTag";
-        // 循环发送消息。
-        Message msg = new Message(
+        Message message = new Message(
             // 普通消息所属的 Topic ，切勿使用普通消息的 Topic 来收发其他类型的消息。
             topic,
-            // Message Tag 可理解为 Gmail 中的标签，对消息进行再归类，方便 Consumer 指定过滤条件在消息队列RocketMQ版的服务器过滤。
+            // Message Tag 可理解为 Gmail 中的标签，对消息进行再归类，方便 Consumer 指定过滤条件在消息队列 RocketMQ 版的服务器过滤。
             tag,
-            // Message Body可以是任何二进制形式的数据，消息队列 RocketMQ 版不做任何干预。
+            // Message Body 可以是任何二进制形式的数据，消息队列 RocketMQ 版不做任何干预。
             // 需要 Producer 与 Consumer 协商好一致的序列化和反序列化方式。
             "This is a NORMAL message for RocketMQ".getBytes());
         // 设置代表消息的业务关键属性，请尽可能全局唯一。
         // 以方便您在无法正常收到消息情况下，可通过消息队列RocketMQ版控制台查询消息并补发。
         // 注意：不设置也不会影响消息正常收发。
         String key = "yourMessageKey";
-        msg.setKey(key);
+        message.setKey(key);
 
         try {
-            SendResult sendResult = producer.send(msg);
+            SendResult sendResult = producer.send(message);
             String messageId = sendResult.getMessageId();
             logger.info("Send NORMAL message successfully, topic={}, messageId={}", topic, messageId);
         } catch (Exception e) {
