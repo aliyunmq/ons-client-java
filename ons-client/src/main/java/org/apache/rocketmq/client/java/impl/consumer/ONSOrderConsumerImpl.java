a24d8fea6b7eba6fa8657f1a396dac3daf2bf7c6
Aaron Ai <yangkun.ayk@alibaba-inc.com>
2023-05-16 16:28:03 +0800

    WIP: implement broadcasting consumption for order consumer

--- ons-client/src/main/java/org/apache/rocketmq/client/java/impl/consumer/ONSOrderConsumerImpl.java
+++ ons-client/src/main/java/org/apache/rocketmq/client/java/impl/consumer/ONSOrderConsumerImpl.java
@@ -1,5 +1,6 @@
 package org.apache.rocketmq.client.java.impl.consumer;
 
+import com.aliyun.openservices.ons.api.Message;
 import com.aliyun.openservices.ons.api.MessageSelector;
 import com.aliyun.openservices.ons.api.OffsetStore;
 import com.aliyun.openservices.ons.api.exception.ONSClientException;
@@ -8,55 +9,107 @@ import com.aliyun.openservices.ons.api.order.MessageOrderListener;
 import com.aliyun.openservices.ons.api.order.OrderAction;
 import com.aliyun.openservices.ons.api.order.OrderConsumer;
 import com.aliyun.openservices.ons.client.UtilAll;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.rocketmq.client.apis.consumer.ConsumeResult;
 import org.apache.rocketmq.client.apis.consumer.FilterExpression;
-import org.apache.rocketmq.client.apis.consumer.FilterExpressionType;
+import org.apache.rocketmq.client.apis.message.MessageView;
 import org.apache.rocketmq.client.java.misc.ClientId;
+import org.apache.rocketmq.client.java.misc.ExecutorServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ONSOrderConsumerImpl extends ONSPushConsumer implements OrderConsumer {
     private static final Logger log = LoggerFactory.getLogger(ONSOrderConsumerImpl.class);
 
+    private final Map<String, FilterExpression> filterExpressionTable;
+    private final Map<String, MessageOrderListener> messageOrderListenerTable;
+
     public ONSOrderConsumerImpl(final Properties properties) {
         super(properties);
+        this.filterExpressionTable = new HashMap<>();
+        this.messageOrderListenerTable = new HashMap<>();
         // Broadcast consumption by order consumers will degenerate into unordered consumption.
         pushConsumer.getPushConsumerSettings().fifo = messageModel.equals(MessageModel.CLUSTERING);
     }
 
     @Override
     public void start() {
-        final ClientId clientId = pushConsumer.getClientId();
+        // For clustering consumption mode.
+        if (MessageModel.CLUSTERING.equals(this.messageModel)) {
+            try {
+                if (this.started.compareAndSet(false, true)) {
+                    log.info("Begin to start the ONS order consumer[clustering], clientId={}", clientId);
+                    this.pushConsumer.startAsync().awaitRunning();
+                    log.info("ONS order consumer[clustering] starts successfully, clientId={}", clientId);
+                    return;
+                }
+                log.warn("ONS order consumer[clustering] has been started before, clientId={}", clientId);
+                return;
+            } catch (Throwable t) {
+                log.error("Failed to start the ONS order consumer[clustering], clientId={}", clientId);
+                throw new ONSClientException(t);
+            }
+        }
+        // For broadcasting consumption mode.
         try {
-            if (this.started.compareAndSet(false, true)) {
-                log.info("Begin to start the ONS order push consumer, clientId={}", clientId);
-                this.pushConsumer.startAsync().awaitRunning();
-                log.info("ONS order push consumer starts successfully, clientId={}", clientId);
+            if (!this.started.compareAndSet(false, true)) {
+                log.warn("ONS order consumer[broadcasting] has been started before, clientId={}", clientId);
                 return;
             }
-            log.warn("ONS order push consumer has been started before, clientId={}", clientId);
+            log.info("Begin to start the ONS order consumer[broadcasting], clientId={}", clientId);
+            this.pullConsumer.startAsync().awaitRunning();
+            log.info("ONS order consumer[broadcasting] starts successfully, clientId={}", clientId);
         } catch (Throwable t) {
-            log.error("Failed to start the ONS order push consumer, clientId={}", clientId);
+            log.error("Failed to start the ONS order consumer[broadcasting], clientId={}", clientId);
             throw new ONSClientException(t);
         }
+        for (Map.Entry<String, FilterExpression> entry : filterExpressionTable.entrySet()) {
+            final String topic = entry.getKey();
+            try {
+                final TopicMessageQueueChangeListenerImpl listener = new TopicMessageQueueChangeListenerImpl(
+                    pullConsumer, filterExpressionTable);
+                pullConsumer.registerMessageQueueChangeListenerByTopic(topic, listener);
+            } catch (Throwable t) {
+                log.error("Failed to register message queue listener, clientId={}", clientId, t);
+            }
+        }
+        this.messagePollingExecutor.submit(new MessagePollingTask());
     }
 
     @Override
     public void shutdown() {
-        final ClientId clientId = pushConsumer.getClientId();
+        // For clustering consumption mode.
+        if (MessageModel.CLUSTERING.equals(this.messageModel)) {
+            try {
+                if (this.started.compareAndSet(true, false)) {
+                    log.info("Begin to shutdown the ONS order consumer[clustering], clientId={}", clientId);
+                    this.pushConsumer.stopAsync().awaitTerminated();
+                    log.info("Shutdown ONS order consumer[clustering] successfully, clientId={}", clientId);
+                    return;
+                }
+                log.warn("ONS order consumer[clustering] has been shutdown before, clientId={}", clientId);
+                return;
+            } catch (Throwable t) {
+                log.error("Failed to shutdown the ONS order consumer[clustering], clientId={}", clientId, t);
+            }
+        }
+        // For broadcasting consumption mode.
         try {
             if (this.started.compareAndSet(true, false)) {
-                log.info("Begin to shutdown the ONS order push consumer, clientId={}", clientId);
-                this.pushConsumer.stopAsync().awaitTerminated();
-                log.info("Shutdown ONS order push consumer successfully, clientId={}", clientId);
+                log.info("Begin to shutdown the ONS order consumer[broadcasting], clientId={}", clientId);
+                this.pullConsumer.stopAsync().awaitTerminated();
+                messagePollingExecutor.shutdown();
+                ExecutorServices.awaitTerminated(messagePollingExecutor);
+                log.info("Shutdown ONS order consumer[broadcasting] successfully, clientId={}", clientId);
                 return;
             }
-            log.info("ONS order push consumer has been shutdown before, clientId={}", clientId);
+            log.warn("ONS order consumer[broadcasting] has been shutdown before, clientId={}", clientId);
         } catch (Throwable t) {
-            log.error("Failed to shutdown the ONS order push consumer, clientId={}", clientId);
-            throw new ONSClientException(t);
+            log.error("Failed to shutdown the ONS order consumer[clustering], clientId={}", clientId, t);
         }
     }
 
@@ -82,6 +135,9 @@ public class ONSOrderConsumerImpl extends ONSPushConsumer implements OrderConsum
         if (null == listener) {
             throw new ONSClientException("Listener is null, please set it");
         }
+        FilterExpression expression = UtilAll.convertFilterExpression(selector);
+        filterExpressionTable.put(topic, expression);
+        messageOrderListenerTable.put(topic, listener);
         pushConsumer.messageListener = messageView -> {
             if (!topic.equals(messageView.getTopic())) {
                 return pushConsumer.messageListener.consume(messageView);
@@ -99,19 +155,41 @@ public class ONSOrderConsumerImpl extends ONSPushConsumer implements OrderConsum
                     return ConsumeResult.FAILURE;
             }
         };
-        FilterExpression expression;
-        switch (selector.getType()) {
-            case TAG:
-                expression = new FilterExpression(selector.getSubExpression(), FilterExpressionType.TAG);
-                break;
-            case SQL92:
-            default:
-                expression = new FilterExpression(selector.getSubExpression(), FilterExpressionType.SQL92);
-        }
         try {
             pushConsumer.subscriptionExpressions.put(topic, expression);
         } catch (Throwable t) {
             throw new ONSClientException(t);
+        }
+    }
+
+    class MessagePollingTask implements Runnable {
+        @Override
+        public void run() {
+            final ClientId clientId = pullConsumer.getClientId();
+            while (started.get()) {
+                try {
+                    final List<MessageView> messageViews = pullConsumer.poll(MESSAGE_BROADCASTING_POLLING_DURATION);
+                    for (MessageView messageView : messageViews) {
+                        final String topic = messageView.getTopic();
+                        final MessageOrderListener listener = messageOrderListenerTable.get(topic);
+                        if (null == listener) {
+                            log.error("MessageListener is null, message={}", messageView);
+                            continue;
+                        }
+                        final Message message = UtilAll.convertMessage(messageView);
+                        final ConsumeOrderContext context = new ConsumeOrderContext();
+                        polledMessageConsumptionExecutor.submit(() -> {
+                            try {
+                                listener.consume(message, context);
+                            } catch (Throwable t) {
+                                log.error("Exception raised while consuming message, clientId={}", clientId, t);
+                            }
+                        });
+                    }
+                } catch (Throwable t) {
+                    log.error("Exception raised while polling message, clientId={}", clientId, t);
+                }
+            }
         }
     }
 }
